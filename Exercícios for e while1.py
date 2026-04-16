59807e45988d4b24c85d62ce9d8435da60202534
Melissa Santos <melissilva123@gmail.com>
2026-04-16 11:57:59 -0300

    Update Exercícios for e while1.py

--- Exercícios for e while1.py
+++ Exercícios for e while1.py
@@ -45,4 +45,28 @@ while contador >= 0:
         print("Estoque esgotado")
     contador -= 1
 
+contador = 10
+
+while contador >= 1:
+    if contador % 2 == 0:
+        print(f"Faltam apenas {contador} segundos - Não perca essa oportunidade!")
+    else:
+        print(f"A contagem continua: {contador} segundos restantes.")
+    contador = contador - 1
+
+print("Aproveite a promoção agora!")
+
+livros = [
+    {"nome": "1984", "estoque": 5},
+    {"nome": "Dom Casmurro", "estoque": 0},
+    {"nome": "O Pequeno Príncipe", "estoque": 3},
+    {"nome": "O Hobbit", "estoque": 0},
+    {"nome": "Orgulho e Preconceito", "estoque": 2}
+]
+
+for livro in livros:
+    if livro["estoque"] == 0:
+        continue
+    print(f"Livro disponível: {livro["nome"]}")
+
 
