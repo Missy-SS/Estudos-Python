6fbd908fe7eacb4b2c1ec6dd447a03e89b88b095
Melissa Santos <melissilva123@gmail.com>
2026-05-13 21:53:55 -0300

    Create Exercícios Lista A.py

---
+++ Exercícios Lista A.py
@@ -0,0 +1,96 @@
+#Exercício 1
+digitos = []
+pesos = [6,5,4,3,2]
+multiplicacao = []
+soma = []
+
+
+numero = int(input("Digite um núemro de 5 dígitos: "))
+
+while  99999 > numero < 10000:
+    print(f"Valor inválido!\nDigite um valor com 5 dígitos!")
+    numero = int(input("Digite um número de 5 dígitos: "))
+
+digitos.append(numero)
+
+separacao = [int(d) for d in str(numero)]
+
+print(separacao)
+
+for i in range(len(separacao)):
+    multiplicacao.append(pesos[i]*separacao[i])
+
+print(multiplicacao)
+
+soma = sum(multiplicacao)
+
+print(soma)
+
+somatoria = soma % 7
+
+print(somatoria)
+
+print(f"{numero}-{somatoria}")
+
+#Exercício 2
+
+#listas
+
+digitos = []
+pesos = [6,5,4,3,2]
+multiplicacao = []
+soma = []
+códigos = []
+arquivo = "Código_Verificador.txt"
+
+numero = int(input("Digite um número: "))
+
+while numero == 0:
+    print(f"Valor inválido!")
+    numero = int(input("Digite um número: "))
+    
+while numero != 0:
+    if 10000 < numero < 30000:
+            
+        digitos.append(numero)
+
+        separacao = [int(d) for d in str(numero)]
+
+        print(separacao)
+
+        for i in range(len(separacao)):
+            multiplicacao.append(pesos[i]*separacao[i])
+
+        print(multiplicacao)
+
+        soma = sum(multiplicacao)
+
+        print(soma)
+
+        somatoria = soma % 7
+
+        print(somatoria)
+
+        print(f"{numero}-{somatoria}")
+        
+        códigos.append(f"{numero}-{somatoria}")
+        
+
+        with open(arquivo, 'a', encoding='utf-8') as file:
+            for codigo in códigos:
+                linha = f"{numero}-{somatoria}\n"
+        
+        # Grava no arquivo
+            file.write(linha)
+            print(f"Gravado: {linha.strip()}")
+            
+        print("Código gravado com sucesso!")
+                
+        multiplicacao.clear()
+        
+        numero = int(input("Digite um Código: "))
+    else:
+        print("Código Inválido!")
+
+
+
